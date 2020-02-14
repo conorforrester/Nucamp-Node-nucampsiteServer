a0caf03efa3c749aed39ee8cba78a1f2aa9d0a17
Conor Forrester <conor.forrester214@gmail.com>
2020-02-13 20:37:31 -0500

    Express REST API with MongoDB and Mongoose Part 1

---
+++ models/campsite.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; //making shorthand to the mongoose.schema function, so we can refer to it as schema instead of mongoose.schema
+
+require('mongoose-currency').loadType(mongoose);    //loads currency type into mongoose
+const Currency = mongoose.Types.Currency;   //shorthand for mongoose.TYpes.Currency
+
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+
+const campsiteSchema = new Schema({ //instantiates new object named campsiteSchema
+    name: {
+        type: String,
+        required: true, //document requires a name property
+        unique: true    //name must be unique
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    elevation: {
+        type: Number,
+        required: true
+    },
+    cost: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    comments: [commentSchema]   //comments property causes every Campsite document to be able to contain mutliple comment documents stored within an array
+}, {
+    timestamps: true    //timestamps - causes mongoose to add two properties to the schema called createdAt and updatedAt
+});
+
+//create model, Campsite, for the data
+const Campsite = mongoose.model('Campsite', campsiteSchema);
+
+module.exports = Campsite;
