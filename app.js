b4304c8da7d8551cf896a3fe9ac36069e0490afa
Conor Forrester <conor.forrester214@gmail.com>
2020-02-19 08:23:34 -0500

    Express Sessions Part 2

--- app.js
+++ app.js
@@ -37,6 +37,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321')); //using signed cookies, so we provide the cookie parser with a secret key as an argument, can be anything
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 app.use(session({
   name: 'session-id',
   secret: '12345-67890-09876-54321',
@@ -48,35 +51,19 @@ app.use(session({
 //authentication
 function auth(req, res, next) {
   console.log(req.session);
-  if (!req.session.user) {
-    const authHeader = req.headers.authorization; //get authorization header
-    if (!authHeader) {  //no authentication information returned, user has not entered credentials
-        const err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic'); //lets client know that server is requesting authentication and that the auth method being requested is basic
-        err.status = 401;
-        return next(err);
-    }
 
-    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':'); //use Node global, Buffer, to parse out the username and password into a new "auth" array
-    const user = auth[0];
-    const pass = auth[1];
-    if (user === 'admin' && pass === 'password') {
-      req.session.user = 'admin';
-      return next(); // authorized
-        } else {
-            const err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
+  if (!req.session.user) {
+      const err = new Error('You are not authenticated!');
+      err.status = 401;
+      return next(err);
+  } else {
+      if (req.session.user === 'authenticated') {
+          return next();
       } else {
-        if (req.session.user === 'admin') {
-            return next();  //authorized
-        } else {
-        const err = new Error('You are not authenticated!');
-        err.status = 401;
-        return next(err);
-    }
+          const err = new Error('You are not authenticated!');
+          err.status = 401;
+          return next(err);
+      }
   }
 }
 
@@ -84,8 +71,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,85 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const User = require('../models/user');
+
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
+});
+
+router.post('/signup', (req, res, next) => {  //POST route allows new user to register on website
+    User.findOne({username: req.body.username}) //check if username already exists
+    .then(user => { //use a promise to deal with the response
+        if (user) { //username exists already
+            const err = new Error(`User ${req.body.username} already exists!`);
+            err.status = 403;
+            return next(err);
+        } else {  //user variable is null, undefined or something other than user document, and user is allowed to create the username
+            User.create({
+                username: req.body.username,
+                password: req.body.password})
+            .then(user => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({status: 'Registration Successful!', user: user});
+            })
+            .catch(err => next(err));
+        }
+    })
+    .catch(err => next(err));
+});
+
+router.post('/login', (req, res, next) => {
+    if(!req.session.user) { //user is not logged in
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader) {
+            const err = new Error('You are not authenticated!');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            return next(err);
+        }
+      
+        const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+        const username = auth[0];
+        const password = auth[1];
+      
+        User.findOne({username: username})  //check if username exists so we can match it in the document
+        .then(user => {
+            if (!user) {
+                const err = new Error(`User ${username} does not exist!`);
+                err.status = 403;
+                return next(err);
+            } else if (user.password !== password) {  //password does not match what is in the document
+                const err = new Error('Your password is incorrect!');
+                err.status = 403;
+                return next(err);
+            } else if (user.username === username && user.password === password) {  //confirm both username and password match
+                req.session.user = 'authenticated';
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end('You are authenticated!')
+            }
+        })
+        .catch(err => next(err));
+    } else {  //user is already logged in
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('You are already authenticated!');
+    }
+});
+
+router.get('/logout', (req, res, next) => {
+    if (req.session) {  //is user is logged in, then kill the session
+      req.session.destroy();
+      res.clearCookie('session-id');
+      res.redirect('/');
+    } else {  //if user is not logged in, reply with...
+      const err = new Error('You are not logged in!');
+      err.status = 403;
+      return next(err);
+    }
 });
 
 module.exports = router;
