aa4136d2189692b4dc0e6340eb3ac3a9fd50aedc
Conor Forrester <conor.forrester214@gmail.com>
2020-02-20 08:16:06 -0500

    Passport JWT

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session); //require is returning session-file-store function as the return value and then immediately calling the session param list
 const passport = require('passport');
-const authenticate = require('./authenticate');
+const config = require('./config');
 
-const url = 'mongodb://localhost:27017/nucampsite';
+
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
     useFindAndModify: false,
@@ -39,36 +37,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321')); //using signed cookies, so we provide the cookie parser with a secret key as an argument, can be anything
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()  //save to server hard disk instead of running in application memory
-}));
-
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-
-//authentication
-function auth(req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');                       
-      err.status = 403;
-      return next(err);
-  } else {
-      return next();
-  }
-}
-
-app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321', //assign to JSON web token
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite' //URL for mongoDB server
+}
